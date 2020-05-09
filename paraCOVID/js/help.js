849bdd70cf0fd136e0f2a554a50ce66e3824fc85
Scott Heston <sheston@gatech.edu>
2020-05-09 09:36:41 -0400

    paraCOVID init commit

---
+++ paraCOVID/js/help.js
@@ -0,0 +1,8 @@
+function clearMain() {
+    // Adds an element to the document
+    console.log("clearing");
+    var p = document.getElementsByClassName("del");
+    for (var e of p) {
+      e.style.display = "none";
+    }
+}
---
+++ paraCOVID/js/translation.js
@@ -0,0 +1,31 @@
+var strings = ["Get free legal help"];
+
+function load_Chinese() {
+    // Adds an element to the document
+    strings = ["获得免费的法律帮助"];
+}
+
+function load_Spanish() {
+    // Adds an element to the document
+    strings = ["Obtenga ayuda legal gratis"];
+}
+
+function load_Russian() {
+    // Adds an element to the document
+    strings = ["Получите бесплатную юридическую помощь"];
+}
+
+function load_English() {
+    // Adds an element to the document
+    strings = ["Get free legal help"];
+}
+
+function update_language() {
+  for (var i of [0]) {
+    var elem = document.getElementsByClassName(i.toString());
+    if (elem.length > 0) {
+      elem[0].innerHTML = strings[i.toString()];
+    }
+  }
+
+}
