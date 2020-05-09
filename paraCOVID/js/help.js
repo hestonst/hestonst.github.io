01e20c9d498a91b6338a07b0e3ada7cf4f056d5d
Scott Heston <sheston@gatech.edu>
2020-05-09 17:38:27 -0400

    New version

--- paraCOVID/js/help.js
+++ paraCOVID/js/help.js
@@ -9,13 +9,22 @@ function clearMain() {
 
 var current_form = 0
 
+function back() {
+  current_form -= 1;
+}
+
+var head
+var blurb
+var cont
+
 function next() {
+  clearMain();
   if (current_form == 0) {
     // Populate the form, display first information
     c = document.getElementsByClassName("content")[0];
     var form = document.createElement('div');
     var div = document.createElement('div');
-    var cont = document.createElement('a');
+    cont = document.createElement('a');
     var back = document.createElement('a');
     cont.classList.add("button");
     cont.classList.add("2");
@@ -23,6 +32,7 @@ function next() {
     cont.href = "#";
     cont.style.textAlign = "center";
     cont.innerHTML = "Continue";
+    cont.onclick = next;
     back.classList.add("button");
     back.classList.add("1");
     back.classList.add("icons");
@@ -33,21 +43,22 @@ function next() {
     div.appendChild(back);
     div.appendChild(cont);
     form.appendChild(div);
-    var head = document.createElement('h1');
+    head = document.createElement('h1');
     head.innerHTML = "Connect with those in need via modern web forms available 24/7 on mobile or a computer: walk through an early demo of our website."
-    var blurb = document.createElement('p');
-    blurb.innerHTML = "This demo will ask you for some basic information and then autofill a PDF with that information. You can use this functionality in the languages shown above. Our goal is to automate COVID-related paralegal work so that pro bono law groups can aid people under duress in these dificult times."
-
-
-
-
-
+    blurb = document.createElement('p');
+    blurb.innerHTML = "This demo will ask you for some basic information and then autofill a PDF with that information. You can use this functionality in the languages shown above. Our goal is to automate COVID-related paralegal work so that pro bono law groups can provide people with aid efficiently under duress in these dificult times."
 
     form.appendChild(head);
     form.appendChild(blurb);
     form.appendChild(div);
     c.appendChild(form);
+
+  }
+  if (current_form == 1) {
+    head.innerHTML = '"Courage – you develop courage by doing small things like just as if you wouldn’t want to pick up a 100-pound weight without preparing yourself --Maya Angelou"'
+    blurb.innerHTML = "We're passionate about making an interface that is friendly and reaffirming because we believe many people are having to ask for help for the first time in their lives. We give you the tools to connect with populations in need no matter their language, struggles with homelessness, or the crisis they are facing."
   }
   current_form += 1;
+
 
 }
